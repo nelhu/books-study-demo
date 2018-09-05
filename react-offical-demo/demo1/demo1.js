bd8d244f790f538bc21c932ebec0c81608cd9dbf
huxuezi <xuezierhu@gmail.com>
2018-09-05 14:01:39 +0800

    update

---
+++ react-offical-demo/demo1/demo1.js
@@ -0,0 +1,21 @@
+console.log('referenced');
+
+// const e = React.createElement;
+// var inner =  e(
+//     'button',
+//      {
+//         onClick: () => console.log('clicked'),
+//      },
+//     'like');
+// ReactDOM.render(inner, document.getElementById("root"));
+
+// const name = 'nelhu';
+// const element = <h1>hi, {name}</h1>;
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+  
